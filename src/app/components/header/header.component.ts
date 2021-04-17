706eecd0c27245eb4d356140d81dcc0a1e3a7307
Fede <dfedericovera@gmail.com>
2021-04-17 20:54:22 -0300

    Email del usuario en header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  email:string="JUANITO";
+
+  constructor(private authService:AuthService) {
+    this.email = this.authService.user.email;
+   }
 
   ngOnInit(): void {
   }
